330ba6d234c1a1534219e60446ed4489af8b67cb
AugustoAleGon <aagg83556@hotmail.com>
2022-07-30 11:17:27 -0500

    :pencil: add custom rules in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,13 +8,18 @@ module.exports = {
     'standard'
   ],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      module: true
+    }
   },
   plugins: [
     'vue'
   ],
   rules: {
+    'space-before-function-paren': 0,
+    'vue/multi-word-component-names': 0
   },
   ignorePatterns: ['src/test/*', './.nuxt'], // <<< ignore all files in test folder
 }
