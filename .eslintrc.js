a3519867853dac62832d77973793cedd330522f5
edcalderon <edwardca12@hotmail.com>
2022-07-30 09:20:06 -0500

    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'vue'
+  ],
+  rules: {
+  },
+  ignorePatterns: ['src/test/*', './.nuxt'], // <<< ignore all files in test folder
+}
